fbd982a91c6009496459d6637bf9b9caebac1d65
panamadalina <madalina.pana1997@gmail.com>
2023-02-28 18:18:23 +0200

    no message

--- app/index.js
+++ app/index.js
@@ -8,8 +8,40 @@ app.use(morgan('combined'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 });
+// app.get('/hello', (req, res) => {
+//   var nume = req.form.get('name')
+//   res.sendFile(__dirname + '/hello.html')
+// });
 
-var listener = app.listen(process.env.PORT || 80, function() {
+var listener = app.listen(process.env.PORT || 92, function() {
  console.log('listening on port ' + listener.address().port);
 });
 
+
+
+/*
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for helo page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+   app.run()
+*/
